b64acfbcf7edf61a7306e2307f0b039e46896756
Prolomon <taiwooyetade67@gmail.com>
2026-07-31 11:28:47 -0700

    Corrected the date number type error

--- admin/src/app/(main)/payment/[identifier]/checkout/page.tsx
+++ admin/src/app/(main)/payment/[identifier]/checkout/page.tsx
@@ -137,8 +137,12 @@ export default function PaymentPage() {
 
     // Calculate days overdue
     let daysOverdue = 0;
+    // if (currentDate > paymentDate) {
+    //     const diffTime = currentDate - paymentDate;
+    //     daysOverdue = Math.floor(diffTime / (1000 * 60 * 60 * 24)); // convert ms → days
+    // }
     if (currentDate > paymentDate) {
-        const diffTime = currentDate - paymentDate;
+        const diffTime = currentDate.getTime() - paymentDate.getTime(); // ✅ use getTime()
         daysOverdue = Math.floor(diffTime / (1000 * 60 * 60 * 24)); // convert ms → days
     }
 
