4e712443dcb4c90c4238523e9b12857b0e63c2b4
Taiwo Oyetade Solomon <97153987+Prolomon@users.noreply.github.com>
2026-07-31 07:26:15 -0700

    corrected the Qrcode link

--- admin/src/app/admin/demands/[id]/page.tsx
+++ admin/src/app/admin/demands/[id]/page.tsx
@@ -261,7 +261,7 @@ function DemandDetailPage() {
   }
 
   // Generate QR code URL
-  const qrData = `https://urms.afriverge.com/payment?m=${demand?.member?.uid}`;
+  const qrData = `https://urms.afriverge.com/payment/${demand?.payment?.reference}/checkout`;
   const qrCodeUrl = `https://quickchart.io/qr?text=${encodeURIComponent(qrData)}&size=240`;
 
   return (
